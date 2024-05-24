2a2b02da0e0583b95e1fae50f635dfac65a96b01
SatyamKumar <kumarsatyam04.2000@gmail.com>
2024-05-24 18:57:25 +0530

    initial file setup completed for next and node

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors({
+    origin: [process.env.PUBLIC_URL],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+}))
+
+app.use(express.json())
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
